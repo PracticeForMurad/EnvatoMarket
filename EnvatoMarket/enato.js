91e13281c588f37826a9bd954c1a15aeb8197331
PracticeForMurad <muradme414@gmail.com>
2025-05-11 17:43:12 +0400

    17:41 Branch elave et buttonlar

--- EnvatoMarket/enato.js
+++ EnvatoMarket/enato.js
@@ -1,9 +1,21 @@
-const addPhoto = () => {
-  const imgUrl = "https://undsgn.com/uncode/homepages/classic-agency/";
-  const resimAlaniDOM = document.getElementById('photo');
-  const imgDOM = document.createElement("img");
-  imgDOM.setAttribute("height", 415);
-  imgDOM.setAttribute("width", 738);
-  imgDOM.setAttribute("src", imgUrl)
-  resimAlaniDOM.appendChild(imgDOM);
+const words = ["landing", "magazines", "website", "portfolio", "e-shop"];
+let index = 0;
+const wordElement = document.getElementById("changing-word");
+
+wordElement.textContent = words[0];
+
+function changeWord() {
+  wordElement.style.animation = 'none';
+  
+  index = (index + 1) % words.length;
+  
+  void wordElement.offsetWidth;
+  
+  wordElement.textContent = words[index];
+  
+  wordElement.style.animation = 'fadeSlide 2s ease-in-out';
 }
+
+setTimeout(() => {
+  setInterval(changeWord, 2000);
+}, 500);
